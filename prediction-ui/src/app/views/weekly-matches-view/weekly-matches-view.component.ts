77dbf1151a6e827fa2e3fb0ef36c40eb04b7b417
Mohammad Javad Rahmati <mj.rahmati@rha.local>
2021-05-29 11:56:57 +0430

    add weekly-matches-view

---
+++ prediction-ui/src/app/views/weekly-matches-view/weekly-matches-view.component.ts
@@ -0,0 +1,40 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {RestClientService} from "../../services/rest-client.service";
+import {SiteInfo} from "../new-site/new-site.component";
+import {PianaStorageService} from "../../services/piana-storage.service";
+import {SiteCategoryService} from "../../services/site-category.service";
+import {WeeklyMatchesService} from "../../services/weekly-mathes.service";
+import {WeeklyMatchesCompetitionService} from "../../services/weekly-mathes-competition.service";
+import {ActivatedRoute} from "@angular/router";
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-weekly-matches-view',
+  templateUrl: './weekly-matches-view.component.html',
+  styleUrls: ['./weekly-matches-view.component.css']
+})
+export class WeeklyMatchesComponent implements OnInit {
+  @Input() weeklyMatchesId: number;
+  private subWeeklyMatchesId: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private pianaStorageService: PianaStorageService,
+    private restClientService: RestClientService,
+    public weeklyMatchesCompetitionService: WeeklyMatchesCompetitionService,
+    private siteCategoryService: SiteCategoryService) {
+  }
+
+  ngOnInit(): void {
+    this.subWeeklyMatchesId = this.route.params.subscribe(params => {
+      this.weeklyMatchesId = +params['weeklyMatchesId']; // (+) converts string 'id' to a number
+      // In a real app: dispatch action to load the details here.
+      console.log(this.weeklyMatchesId)
+      this.weeklyMatchesCompetitionService.fetch(this.weeklyMatchesId);
+    });
+  }
+}
